0e0c52c60706b960c3890a2161aa4b6187d111c6
renaldosilva <renaldosilva954@gmail.com>
2023-04-21 15:27:19 -0300

    feat: adding image load

--- snakegame/__main__.py
+++ snakegame/__main__.py
@@ -1,23 +1,26 @@
 import pygame
 
-from snakegame import constants
+from snakegame import constants, util
 from snakegame.game.game import Game
 from snakegame.game.basic_piece import BasicPiece
 
 
+pygame.init()
+
+
 # Load
-icon = pygame.image.load(constants.WINDOW_ICON_IMAGE_PATH)
+icon = util.load_image(constants.WINDOW_ICON_IMAGE_PATH)
 
 # Window
+window = pygame.display.set_mode(constants.WINDOW_DIMENSIONS)
 pygame.display.set_caption(constants.GAME_NAME)
 pygame.display.set_icon(icon)
-canvas = pygame.display.set_mode(constants.WINDOW_DIMENSIONS)
 
 # Clock
 clock = pygame.time.Clock()
 
 # Basic game features
-basic_piece = BasicPiece(canvas, clock)
+basic_piece = BasicPiece(window, clock)
 
 # Game
 game = Game(basic_piece)
--- snakegame/util.py
+++ snakegame/util.py
@@ -1,5 +1,7 @@
 import pygame
-from pygame import display
+from pygame import display, Surface, SurfaceType
+
+from snakegame import validation
 
 
 def get_system_display_dimensions() -> tuple[int, int]:
@@ -35,3 +37,68 @@ def configure_event(event: int, milliseconds: int) -> int:
     pygame.time.set_timer(event, milliseconds)
 
     return event
+
+
+def load_images(image_paths: list[str], dimensions: tuple[int, int]) -> list | list[Surface | SurfaceType]:
+    """
+    Load a list of images. An empty list is returned if no path exists.
+
+    Parameters
+    ----------
+    image_paths : list[str]
+        The paths of the images.
+    dimensions : tuple[int, int]
+        The width and height dimensions.
+
+    Returns
+    -------
+    list | list[Surface | SurfaceType]
+        The list.
+
+    Raises
+    ------
+    ValueError
+        If 'dimensions' are not positive values.
+    FileNotFoundError
+        If any image path is not found.
+    """
+    images = []
+    if image_paths:
+        for image_path in image_paths:
+            image = load_image(image_path, dimensions)
+            images.append(image)
+
+    return images
+
+
+def load_image(image_path: str, dimensions: tuple[int, int]=(-1, -1)) -> Surface:
+    """
+    Load an image. If the dimensions are not informed, the image is loaded in the original size.
+
+    Parameters
+    ----------
+    image_path : str
+        The path of the image.
+    dimensions : tuple[int, int], optional
+        The width and height dimensions (default is (-1, -1)).
+
+    Returns
+    -------
+    Surface
+        The image.
+
+    Raises
+    ------
+    ValueError
+        If 'dimensions' are not positive values.
+    FileNotFoundError
+        If the 'image_path' is not found.
+    """
+    validation.is_valid_path(image_path, "'image path' not found!")
+    image = pygame.image.load(image_path)
+
+    if dimensions != (-1, -1):
+        validation.is_valid_dimensions(dimensions, "All 'dimensions' must be greater than zero!")
+        image = pygame.transform.scale(image, dimensions)
+
+    return image
