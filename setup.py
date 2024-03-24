f883ec554c677f72a49e91928a36e2ff55994b7a
renaldosilva <renaldo.santino@ccc.ufcg.edu.br>
2024-03-24 01:54:56 -0300

    refactor: removing requirements file

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme:
     long_description = readme.read()
 
-with open("requirements.txt", "r") as requirements:
-    install_requires = [line.strip() for line in requirements]
-
 
 setup(
     name='snakegame',
@@ -17,7 +14,6 @@ setup(
     author='Renaldo Silva Santino',
     description='The classic snake game',
     long_description=long_description,
-    install_requires=install_requires,
     classifiers=[
         "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
